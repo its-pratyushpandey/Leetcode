fe0f7a5ee9156ca22a819a23e8b4a8436b362336
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-08-31 02:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (80.80%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& b) {
-        bool row[9][9] = {false}, col[9][9] = {false}, box[9][9] = {false};
+        bool r[9][9] = {}, c[9][9] = {}, x[9][9] = {};
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (b[i][j] == '.') continue;
-                int num = b[i][j] - '1';
-                int k = (i / 3) * 3 + j / 3;
-                if (row[i][num] || col[j][num] || box[k][num]) return false;
-                row[i][num] = col[j][num] = box[k][num] = true;
+                int v = b[i][j] - '1';
+                int k = (i/3)*3 + j/3;
+                if (r[i][v] || c[j][v] || x[k][v]) return false;
+                r[i][v] = c[j][v] = x[k][v] = true;
             }
         }
         return true;
